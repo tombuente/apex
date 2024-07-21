ffdfa639cfa18c5910b692d9c077a54e5e55c9e4
Tom Bünte <108880623+tombuente@users.noreply.github.com>
2024-07-21 15:02:39 +0200

    Improve generic database functions

--- internal/database/database.go
+++ internal/database/database.go
@@ -12,13 +12,13 @@ import (
 
 func One[T any](ctx context.Context, db *pgx.Conn, query string, args ...any) (T, error) {
 	var defaultT T
-	var i T
+
 	rows, err := db.Query(ctx, query, args...)
 	if err != nil {
 		return defaultT, wrapError(err)
 	}
 
-	i, err = pgx.CollectOneRow[T](rows, pgx.RowToStructByNameLax)
+	i, err := pgx.CollectOneRow[T](rows, pgx.RowToStructByNameLax)
 	if err != nil {
 		return defaultT, wrapError(err)
 	}
@@ -27,19 +27,21 @@ func One[T any](ctx context.Context, db *pgx.Conn, query string, args ...any) (T
 }
 
 func Many[T any](ctx context.Context, db *pgx.Conn, query string, args ...any) ([]T, error) {
+	var defaultT []T
+
 	rows, err := db.Query(ctx, query, args...)
 	if err != nil {
-		return nil, wrapError(err)
+		return defaultT, wrapError(err)
 	}
 	defer rows.Close()
 
 	is, err := pgx.CollectRows[T](rows, pgx.RowToStructByNameLax)
 	if err != nil {
-		return nil, wrapError(err)
+		return defaultT, wrapError(err)
 	}
 
 	if len(is) == 0 {
-		return nil, xerrors.ErrNotFound
+		return defaultT, xerrors.ErrNotFound
 	}
 
 	return is, nil
@@ -52,7 +54,7 @@ func wrapError(err error) error {
 
 	var pgErr *pgconn.PgError
 	if errors.As(err, &pgErr) {
-		return fmt.Errorf("%w: postgres error: message: %v, code : %v", xerrors.ErrInternal, pgErr.Message, pgErr.Code)
+		return fmt.Errorf("%w: postgres error: %v, code : %v", xerrors.ErrInternal, pgErr.Message, pgErr.Code)
 	}
 
 	return xerrors.Join(xerrors.ErrInternal, err)
